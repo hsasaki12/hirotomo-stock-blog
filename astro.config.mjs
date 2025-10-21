e95345dccfeba5f29b9f5f3c6cffae659d9633bf
佐々木博友 <sasaki@sasaki.local>
2025-10-21 23:10:25 +0900

    chore: set site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   integrations: [mdx()],
-  site: 'https://example.com'
+  site: 'https://hirotomo-stock-blog-qni0f7by1-hsasaki12s-projects.vercel.app'
 });
